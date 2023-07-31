4c13788c3c4772f6b87354ec6b33d922472b1f2d
Amir Ghasemian <amirghasemian.ag@gmail.com>
2023-07-31 12:42:54 +0330

    Update tests.py

--- video/tests.py
+++ video/tests.py
@@ -1,14 +1,48 @@
 from django.test import TestCase
 from .models import Video 
+import pytest
+import redis
+import os
+import subprocess
 
-class Test_ffmpeg(TestCase):
-    def test_exitance_of_ffmpeg(self):
-        assert  import ffmpeg
+def is_redis_available(host='localhost', port=6379):
+    try:
+        client = redis.StrictRedis(host=host, port=port, db=0)
 
-    def test_upload_file(self):
-        video = Video(video ='media/video/fak.mp4')
-        asert video.video_240 == 'media/video_240/fak.mp4'
-        asert video.video_360 == 'media/video_360/fak.mp4'
-        
+        client.ping()
+        return True
+    except redis.ConnectionError:
+        return False
+
+def test_redis_availability():
+   
+    assert is_redis_available(), "Redis is not available. Please ensure it is running and accessible."
 
 
+def is_ffmpeg_installed():
+    try:
+        # اجرای دستور ffmpeg -version و بررسی خروجی آن
+        subprocess.run(['ffmpeg', '-version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+@pytest.mark.parametrize("input_quality, output_quality", [("source.mp4", "240"), ("source.mp4", "360")])
+def test_ffmpeg_quality_conversion(input_quality, output_quality):
+    # بررسی وجود ffmpeg
+    assert is_ffmpeg_installed(), "FFmpeg is not installed or accessible. Please ensure it is properly installed."
+
+    # دستور تبدیل کیفیت با ffmpeg
+    input_file = os.path.join("path_to_test_files", input_quality)
+    output_file = os.path.join("path_to_test_files", f"output_{output_quality}.mp4")
+    ffmpeg_cmd = f"ffmpeg -i {input_file} -vf scale={output_quality}:-1 -c:v libx264 -c:a copy {output_file}"
+
+    try:
+        # اجرای دستور تبدیل با ffmpeg
+        subprocess.run(ffmpeg_cmd, shell=True, check=True)
+        # در اینجا می‌توانید اعتبارسنجی‌های دیگری انجام دهید، مانند بررسی اندازه و کیفیت فایل خروجی
+        assert os.path.exists(output_file), f"FFmpeg conversion failed. Output file '{output_file}' not found."
+    finally:
+        # پاک کردن فایل خروجی برای تمیز کردن محیط تست
+        if os.path.exists(output_file):
+            os.remove(output_file)
