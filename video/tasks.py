db16c84d82e65735bc7ef39acdcd327462c87023
Amir Ghasemian <amirghasemian.ag@gmail.com>
2023-08-22 13:22:26 +0330

    Update tasks.py

--- video/tasks.py
+++ video/tasks.py
@@ -1,10 +1,24 @@
 from time import perf_counter
-
 import ffmpeg
+import re
 from celery import shared_task
 from django.conf import settings
-
 from .models import Video
+
+class VideoUtils:
+    @staticmethod
+    def generate_ffmpeg_cmd(video_path, output_name):
+        return (
+            ffmpeg
+            .input(video_path)
+            .output(output_name, bitrate='500k', resolution ='420x240' if "240" in output_name else '640x360')
+        )
+
+    @staticmethod
+    def save_and_remove_files(instance, output_name, video_name):
+        with open(output_name, 'rb') as file:
+            instance.video_240.save(f"{video_name}_240.mp4", File(file)) if "240" in output_name else instance.video_360.save(f"{video_name}_360.mp4", File(file))
+        os.remove(output_name)
 
 
 @shared_task
@@ -14,37 +28,18 @@ def convert(id):
         instance = Video.objects.get(id=id)
     except Video.DoesNotExist:
         return None
-        
+
     video_path = instance.video.path   
     video_name = re.search(r'\/((.+))\.mp4',video_path).group(1)
-    output_name_240 = f'media/video_240/{video_name}_240.mp4'   
-    output_name_360 = f'media/video_360/{video_name}_360.mp4'
-    
-    ffmpeg_cmd_240= (
-        ffmpeg
-        .input(video_path)
-        .output(output_name_240,bitrate='500k',resolution ='420x240')
-    )
-    ffmpeg_cmd_240.run()
+    resolutions = ["240", "360"]
+    for resolution in resolutions:
+        output_name = f'media/video_{resolution}/{video_name}_{resolution}.mp4'
+        instance.url =  settings.MEDIA_ROOT + f"/video_{resolution}/{video_name}.mp4"
+        ffmpeg_cmd = VideoUtils.generate_ffmpeg_cmd(video_path, output_name)
+        ffmpeg_cmd.run()
+        VideoUtils.save_and_remove_files(instance, output_name, video_name)
 
-    ffmpeg_cmd_360= (
-        ffmpeg
-        .input(video_path)
-        .output(output_name_360 ,bitrate='500k',resolution ='640x360')
-    )
-    ffmpeg_cmd_360.run()
-    
-    instance.url_240 =  settings.MEDIA_ROOT+ f"/video_240/{video_name}.mp4"
-    instance.url_360 = settings.MEDIA_ROOT+ f"/video_360/{video_name}.mp4"
     stop_time = perf_counter()
-    delta_time = stop_time - start_time
-    instance.time_to_convert = delta_time
-    
-    with open(output_name_240, 'rb') as file_240, open(output_name_360, 'rb') as file_360:
-        instance.video_240.save(f"{video_name}_240.mp4", File(file_240))
-        instance.video_360.save(f"{video_name}_360.mp4", File(file_360))
-        
-    os.remove(output_name_240)
-    os.remove(output_name_360)
+    instance.time_to_convert = stop_time - start_time
     instance.save()
     return instance
